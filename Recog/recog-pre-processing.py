6e5b6b8b990c1da1c0d01111a409e5ebff3dc67c
samuel.martins <samuel.martins@starline.inf.br>
2022-05-24 10:47:32 -0300

    added folder with images and text recognition script

--- Recog/recog-pre-processing.py
+++ Recog/recog-pre-processing.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pytesseract as pt
 import cv2
 import numpy as np
@@ -12,11 +13,33 @@ def gray_escale(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
+def simple_thresholding(img):
+    print('limiarização simples')
+    return cv2.threshold(
+        img, 180, 255, cv2.THRESH_BINARY
+    )
+
+
+def otsu_thresholding(img):
+    print("limiarização simples com método otsu")
+    return cv2.threshold(
+        img, 0, 255, cv2.THRESH_BINARY | cv2. THRESH_OTSU
+    )
+
+
+home = Path.home()
+path_img = str(Path(home, "Projetos-Python", "OCR", "img", "Test3.png"))
+print(home)
+print(path_img)
+
 config = 'tessdata-dir tessdata'
 
-img = cv2.imread('C:\\Users\\samc3\\Pictures\\Test2.jpg')
-text = pt.image_to_string(img, lang='por', config=config)
+img = cv2.imread(path_img)
 view_image(img)
-print(text)
 
 gray = gray_escale(img)
+view_image(gray)
+
+val, thresh = otsu_thresholding(gray)
+view_image(thresh)
+print("Threshold:", val)
