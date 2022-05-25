422d436cf4f86a4eb0cfbd6d434322add3509ff5
samuel.martins <samuel.martins@starline.inf.br>
2022-05-25 19:56:15 -0300

    added east architeture

--- OCR-East/east_module.py
+++ OCR-East/east_module.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import pytesseract as pt
 from imutils.object_detection import non_max_suppression
 from pathlib import Path
 
@@ -23,6 +24,13 @@ def view_image(img):
 def redimension(img, w, h):
     print("Redimensionamento de imagem")
     return cv2.resize(img, (w, h))
+
+
+def max_redimension(img, ratio):
+    print("Redimensionamento maior")
+    return cv2.resize(
+        img, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_CUBIC
+    )
 
 
 def rectangle(img, x, y, w, h, cor):
@@ -64,3 +72,39 @@ def geometry_calc(x, y, angleData, xData0, xData1, xData2, xData3):
 
 def detections(box, confiance):
     return non_max_suppression(np.array(box), probs=confiance)
+
+
+def gray_escale(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+def otsu_thresholding(img):
+    print("limiarização simples com método otsu")
+    return cv2.threshold(
+        img, 0, 255, cv2.THRESH_BINARY | cv2. THRESH_OTSU
+    )
+
+
+def gauss_thresholding(img):
+    print('limiarização adaptativa gaussiana')
+    return cv2.adaptiveThreshold(
+        img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 9
+    )
+
+
+def erosao(img):
+    print("Erosão")
+    return cv2.erode(img, np.ones((2, 2), np.uint8))
+
+
+def dilatacao(img):
+    print("Dilatação")
+    return cv2.dilate(img, np.ones((3, 3), np.uint8))
+
+
+def invert(img):
+    return 255 - img
+
+
+def extract_text(img, lang, config):
+    return pt.image_to_string(img, lang=lang, config=config)
--- OCR-East/main.py
+++ OCR-East/main.py
@@ -1,9 +1,10 @@
 from turtle import shape
 import east_module as mod
 
-w, h = 832, 480
+#w, h = 832, 480
+w, h = 704, 352
 
-list_path = ["Projetos-Python", "OCR", "img", "caneca.png"]
+list_path = ["Projetos-Python", "OCR", "img", "natural-2.jpg"]
 path = mod.filepath(list_path)
 min_confiance = 0.9
 
@@ -14,7 +15,7 @@ mod.view_image(img)
 original = img.copy()
 
 # Informa os atributos da imagem, como largura, altura e canais RGB
-print(img.shape)
+print("Shape da imagem:", img.shape)
 
 # calcula a proporção da imagem para realizar
 # o preprocessamento para adequar a imagem a arquitetura east
@@ -26,7 +27,7 @@ print("Proporção da imagem:", w_ratio, h_ratio)
 
 # redimensiona a imagem
 img = mod.redimension(img, w, h)
-print(img.shape)
+print("Shape da imagem convertida:", img.shape)
 mod.view_image(img)
 
 # Carregamento da rede neural
@@ -42,9 +43,6 @@ blob = mod.convert_to_blob(img, w, h)
 
 neural.setInput(blob)
 scores, geometry = neural.forward(layers)
-
-print("SCORES\n", scores)
-print("GEOMETRY\n", geometry)
 
 row, col = scores.shape[2:4]
 boxs = []
@@ -67,29 +65,44 @@ for y in range(0, row):
         confiances.append(data_scores[x])
         boxs.append((startX, startY, endX, endY))
 
-print("CONFIANÇAS\n", confiances)
-print("CAIXAS\n", boxs)
-
 detection = mod.detections(boxs, confiances)
-print("DETECÇÕES\n", detection)
+print(f"DETECÇÕES - {len(detection)}\n", detection)
 
 origin_copy = original.copy()
 cor = (255, 100, 0)
 images = []
 
+# detecção do texto
+config_tesseract = 'tessdata-dir tessdata --psm 7'
+margin = 5
+
 for (x, y, w, z) in detection:
-    print(x, y, w, z)
+    print("Posições do bound box:", x, y, w, z)
     x = int(x * w_ratio)
     y = int(y * h_ratio)
     w = int(w * w_ratio)
     z = int(z * h_ratio)
 
     # region of interest
-    roi = origin_copy[y:z, x:w]
-    images.append(roi)
+    roi = origin_copy[y - margin:z + margin, x - margin:w + margin]
+    #roi = origin_copy[y:z, x:w]
+    temp = mod.max_redimension(roi, 1.5)
+    mod.view_image(temp)
+    images.append(temp)
     mod.rectangle(original, x, y, w, z, cor)
 
 mod.view_image(original)
 
 for i in images:
     mod.view_image(i)
+
+    gray = mod.gray_escale(i)
+    mod.view_image(gray)
+
+    val, thresh = mod.otsu_thresholding(gray)
+    mod.view_image(thresh)
+
+    # inv = mod.invert(thresh)
+    # mod.view_image(inv)
+
+    print(mod.extract_text(thresh, "por", config_tesseract))
